63cb7c386ea9c8e8bf47b67265e23373d6fe2273
Jan Waltl <waltl.jan@gmail.com>
2022-06-02 21:53:08 +0200

    Initialize the repository with hello-world

---
+++ include/elphi/hello.hpp
@@ -0,0 +1,6 @@
+#ifndef ELPHI_HELLO_HEADER
+#define ELPHI_HELLO_HEADER
+
+void hello();
+
+#endif /* ifndef ELPHI_HELLO_HEADER */
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "elphi/hello.hpp"
+
+int main(){
+	hello();
+	return 0;
+}
