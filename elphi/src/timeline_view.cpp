d62a20d54f4cfbc0422afd772d2ad069a24bfd2e
Jan Waltl <waltl.jan@gmail.com>
2022-06-05 00:47:02 +0200

    feat: Add basic timeline

---
+++ elphi/src/timeline_view.cpp
@@ -0,0 +1,42 @@
+#include <elphi/timeline_view.hpp>
+#include <fmt/format.h>
+
+
+namespace elphi::view {
+namespace {
+
+/*******************************************************************************
+ * @brief Resolve process name or returns "UNKNOWN".
+ ******************************************************************************/
+std::string
+resolve_name(const SamplingResult& result, ProcId pid) {
+    auto it = result.process_names.find(pid);
+    return it == result.process_names.end() ? "UNKNOWN" : it->second;
+}
+} // namespace
+
+Timeline
+gen_cpu_timelines(const SamplingResult& result) {
+
+    Timeline timeline;
+
+    for (const auto& sample : result.samples) {
+        auto& cpu_timeline = timeline.try_emplace(sample.cpu).first->second;
+
+        // Different execution context -> new slice.
+        if (cpu_timeline.empty() || cpu_timeline.back().pid != sample.pid || cpu_timeline.back().tid != sample.tid) {
+            cpu_timeline.push_back(ThreadTimeSlice{.begin_time = sample.time,
+                                                   .end_time = sample.time,
+                                                   .name = resolve_name(result, sample.pid),
+                                                   .pid = sample.pid,
+                                                   .tid = sample.tid,
+                                                   .cpu = sample.cpu,
+                                                   .cgroup = sample.cgroup});
+        } else {
+            // Prolong the current slice by this sample.
+            cpu_timeline.back().end_time = sample.time;
+        }
+    }
+    return timeline;
+}
+} // namespace elphi::view
