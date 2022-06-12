6bf8d26ab214517b68d6b98868a59b80eb38294b
Jan Waltl <waltl.jan@gmail.com>
2022-06-12 17:52:18 +0200

    chore: Ignore retval due to specified POSIX value

--- elphi/src/utils.cpp
+++ elphi/src/utils.cpp
@@ -8,7 +8,7 @@ std::string
 strerror(int err) {
     std::string buffer;
     buffer.resize(256);
-    strerror_r(err, buffer.data(), buffer.size());
+    (void)strerror_r(err, buffer.data(), buffer.size());
     buffer.resize(std::strlen(buffer.data()));
     return buffer;
 }
