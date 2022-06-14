c8373d03d99aa28d1d20df50c5c3bce946dddf58
Jan Waltl <waltl.jan@gmail.com>
2022-06-14 21:20:59 +0200

    fix: GNU specific strerror_r

--- elphi/src/utils.cpp
+++ elphi/src/utils.cpp
@@ -2,15 +2,15 @@
 
 #include <elphi/utils.hpp>
 
-
 namespace elphi {
 std::string
 strerror(int err) {
     std::string buffer;
     buffer.resize(256);
+    // g++ for some reason defines GNU_SOURCE -> GNU specific implementation
+    // which return through retval is used.
+    // Buffer might or might not be used.
     char* ret = strerror_r(err, buffer.data(), buffer.size());
-    (void)ret;
-    buffer.resize(std::strlen(buffer.data()));
-    return buffer;
+    return std::string{ret};
 }
 } // namespace elphi
