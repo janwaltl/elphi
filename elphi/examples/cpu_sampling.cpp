7ed965a304312a30fba49e54be1b9bd64b35ba83
Jan Waltl <waltl.jan@gmail.com>
2022-08-04 23:31:35 +0200

    docs: Add examples to the build system
    + Fix for the new sampler

---
+++ elphi/examples/cpu_sampling.cpp
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * @file cpu_sampler.cpp
+ * @copyright Copyright 2022 Jan Waltl.
+ * @license	This file is released under ElPhi project's license, see LICENSE.
+ *
+ * Example on how to sample the system activity.
+ ******************************************************************************/
+
+#include <cassert>
+#include <future>
+#include <thread>
+
+#include <fmt/core.h>
+
+#include <elphi/cpu_sampler.hpp>
+
+using namespace std::chrono_literals;
+
+
+constexpr std::size_t frequency = 10;
+
+int
+main() {
+    try {
+        std::promise<elphi::CpuSamplingResult> promise;
+        auto fut = promise.get_future();
+        std::jthread sampling{[&](const auto& tok) {
+            promise.set_value(elphi::sample_cpus_sync(std::vector<elphi::CpuId>{0}, frequency, tok));
+        }};
+
+        std::this_thread::sleep_for(4s);
+        sampling.request_stop();
+        sampling.join();
+        auto samples = fut.get();
+
+        fmt::print("Number of samples {}\n", samples.samples.size());
+    } catch (const elphi::ElphiException& e) {
+        fmt::print("EXCEPTION {}\n", e.what());
+    }
+}
